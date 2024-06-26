bf7fc4c9d4dfaf55c68abac42f42289de9ae6637
SteeveMbr1 <steeve.mombrun@outlook.com>
2024-06-26 18:14:25 +0200

    Fix 'Carousel' ratio and 'Dropdown' transition

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -19,10 +19,8 @@ export default function Dropdown({ title, children }: DropdownProps) {
     return (
         <div className="dropdown">
             <div className="top-bar">
-                <div className="title">{title}</div>
-                <div className="arrow" onClick={(e) => toggleActive(e)}>
-                    <img src={arrow} alt="toggle dropdown" />
-                </div>
+                <div className="title">{title} </div>
+                <div className="arrow" onClick={(e) => toggleActive(e)}>&#10095;</div>
             </div>
             <div className="content">{children}</div>
         </div>
